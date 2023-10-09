c0d7fd24a6cf152f8677eeb03e592ea02e3139b4
Eduardo Hessel <edu.hessel@hotmail.com>
2023-10-09 17:06:33 -0300

    v1.5 Adicionado componente LifeCycle com 3 funções de javascript

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,15 +1,18 @@
 <template> <!--HTML-->
   <PrimeiroComponente /> <!--Componente-->
   <p> O dado do app é: {{ testando }}</p>
+  <LifeCycle />
 </template>
 
 <script> /* Javascript */
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import LifeCycle from './components/LifeCycle.vue'
 
   export default {
     name: 'App',
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
     },
     data() {
       return{
---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template> <!--HTML-->
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+        data() { /* Dados */
+            return {
+                nome: 'Ainda não sei'
+            }
+        },
+        created() { /* Após ser criado os dados alterar dados em 1 segundo (pode ser usado como loading)*/
+            setTimeout(() => {
+                this.nome = 'Matheus'
+            }, 1000);
+        },
+        mounted() { /* Mostrando dados alterar dados em 2 segundos */
+            setTimeout(() => {
+                this.nome = 'Pedro'
+            }, 2000);
+        }
+    }
+</script>
